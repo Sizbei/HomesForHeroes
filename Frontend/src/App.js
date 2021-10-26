31e0a7ae777d79f4716b0f1cec1402fa53027338
Alac Wong <46137625+alacwong@users.noreply.github.com>
2021-10-25 22:09:23 -0400

    Update App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         Raymond I see u tryna finesse hacktoberfest 👀
         </a>
       </header>
     </div>
